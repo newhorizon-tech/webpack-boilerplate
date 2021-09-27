d9626e0a9efffeb734f1340bf4409d219165afd4
newhorizon-tech <alaukikinbox@gmail.com>
2021-09-27 15:01:00 +0530

    Fix webpack config

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
-import './style.scss'
+import './style.scss';
+import './style.css';
 
-console.log("Webpack works!")
+console.log('Webpack works!');
--- webpack.config.js
+++ webpack.config.js
@@ -15,25 +15,33 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       title: 'Output Management',
-      template: './src/index.html'
+      template: './src/index.html',
     }),
   ],
   module: {
     rules: [{
-        test: /\.s[ac]ss$/i,
-        use: [
-          // Creates `style` nodes from JS strings
-          'style-loader',
-          // Translates CSS into CommonJS
-          'css-loader',
-          // Compiles Sass to CSS
-          'sass-loader',
-        ],
-      },
-      {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: 'asset/resource',
-      },
+      test: /\.css$/i,
+      use: [
+        // Creates `style` nodes from JS strings
+        'style-loader',
+        // Translates CSS into CommonJS
+        'css-loader',
+      ],
+    }, {
+      test: /\.s[ac]ss$/i,
+      use: [
+        // Creates `style` nodes from JS strings
+        'style-loader',
+        // Translates CSS into CommonJS
+        'css-loader',
+        // Compiles Sass to CSS
+        'sass-loader',
+      ],
+    },
+    {
+      test: /\.(png|svg|jpg|jpeg|gif)$/i,
+      type: 'asset/resource',
+    },
 
     ],
   },
